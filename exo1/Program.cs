f7ca013091234a4fa3353780d9af01c7eb412233
ElmehdiBLD <elmehdi.belhadri@gmail.com>
2020-09-22 16:11:47 +0200

    Add files via upload

---
+++ exo1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice1
+{
+    class Article
+    {
+        protected string designation;
+        protected int prix;
+
+        public void acheter()
+        {
+
+        }
+    }
+
+    class Livre : Article
+    {
+        protected string isbn;
+        protected int nbPages;
+    }
+
+    class Disque : Article
+    {
+        protected string label;
+    }
+
+    class Video : Article
+    {
+        protected int duree;
+
+        public void afficher()
+        {
+
+        }
+    }
+
+    class Poche : Livre
+    {
+        private string categorie;
+    }
+
+    class Broche : Livre
+    {
+    }
+}
